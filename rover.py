fc6d932510befdac5f4308f0c9f40dcbcd109f9a
Matt Blaszczyk <matthew.blaszczyk1@marist.edu>
2018-01-29 17:41:44 -0500

    Solution to Curiosity rover exercise.

--- rover.py
+++ rover.py
@@ -3,12 +3,12 @@
 # Date: 1/29/2018
 
 # rover.py
-# A program to calculate how long it takes a photo from the Mars Curiosity Rover to reach NASA
+# A program to calculate how long it takes a photo from the Mars Curiosity Rover to reach NASA from 34 million miles away
 
 def mars():
-    miles = eval(input("Enter distance that Mars is from Earth (in miles):"))
+    miles = 34000000
     time = miles * (1/186000)
-    print("It will take the picture", time, "seconds to reach NASA from the Mars Curiosity Rover")
+    print("It will take the picture", time, "seconds to reach NASA from the Mars Curiosity Rover when it is 34 million miles away.")
 
 mars()
 
