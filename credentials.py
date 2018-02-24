a8269409a3008ab0c9d7a84ba3d45e9054a4d64c
Matt Blaszczyk <matthew.blaszczyk1@marist.edu>
2018-02-23 20:29:28 -0500

    Solution to using functions for generating a password and email.

--- credentials.py
+++ credentials.py
@@ -3,21 +3,32 @@
 # Author: Matt Blaszczyk
 # Created: 2/23/2018
 
-def main():
-    # get user's first and last names
+
+def firstName():
     first = input("Enter your first name: ")
+    return(first)
+
+def lastName():
     last = input("Enter your last name: ")
-    uname = first +"."+ last
-    passwd = input("Create a new password: ")
-    if len(passwd) >=8:
-        print("The force is strong in this one…")
-        print("Account configured. Your new email address is",
-              uname + "@marist.edu")
-    elif len(passwd) <8:
-        print("Fool of a Took! That password is feeble! Create a passwords with at least 8 charcters!")
-        passwd = input("Create a new password: ")
-        if len(passwd) >=8:
+    return(last)
+
+def username():
+    for i in range(1):
+        firstN=firstName();
+        lastN=lastName();
+        username = firstN + "." + lastN
+        print("Your Marist username is: ", username)
+        password = input("Create a new password: ")
+        if len(password) >=8:
             print("The force is strong in this one…")
             print("Account configured. Your new email address is",
-              uname + "@marist.edu")
-main()
+                  username + "@marist.edu")
+        elif len(password) <8:
+            print("Fool of a Took! That password is feeble! Create a passwords with at least 8 charcters!")
+            password = input("Create a new password: ")
+            if len(password) >=8:
+                print("The force is strong in this one…")
+                print("Account configured. Your new email address is",
+                  username + "@marist.edu")
+username()
+
